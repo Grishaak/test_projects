550c928d05a74c3fa61ab712b2d865bc2c09f25e
Гребнев Григорий <antonianogroboracevskij@gmail.com>
2023-10-20 20:06:30 +0300

    okey adding some shit

--- exercises_book/exeptions/exeptions.py
+++ exercises_book/exeptions/exeptions.py
@@ -1,6 +1,7 @@
 def main():
     num = [1, 2, 34, 10, 5, 6]
-    num1 = [1, 2, 34, 10, 5, 6, 4, 5]
+    num1 = [1, 2, 34, 10, 5, 6, 4,     5]
+
     # for i in range(len(num)):
 
     return num + num1
--- exercises_book/junk/some_junk.py
+++ exercises_book/junk/some_junk.py
@@ -2,7 +2,35 @@ S = 'shrubbery'
 L = list(S)
 print(L)
 
-B = bytearray(b'orange')
-B.extend(b'juce')
+B = bytearray(b'da')
+B.extend(b'net')
 x = B.decode()
 print(x)
+s = "Spam"
+x = s.find('pa')
+print(x)
+y = s.replace("pa", "xsa")
+print(y)
+
+string = '%s, eggs, and %s' % ('spam', 'SPAM!')
+string2 = '{0}, eggs, and {1}'.format('spam', 'SPAM!')
+string3 = '{}, eggs, and {}'.format('spam', 'SPAM!')
+# print(string)
+# print(string2)
+# string = string.__add__(" nothing here!")
+# print(string)
+# n_str = """132123
+# 12312313
+# 412414
+# 142124"""
+# print(n_str.replace("\n", "_"))
+# string =
+z = u'x'.encode() + b'y'
+print(z.decode())
+L = [123,]
+# list comprehenshion
+M = [[1,2,3],
+     [4,5,6],
+     [7,8,9]]
+col = [row[1] for row in M]
+print(col)
